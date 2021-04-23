66f662056008a410bd39fe492846c6abe4f8d18d
Gerald Southwood <72921662+VertX-G@users.noreply.github.com>
2021-04-23 20:24:00 +0200

    Tower added

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+/*
+
+TODO:
+• simplify role.mason sorts and filters
+• figure out how to use containers
+• figure out how to get creeps to get energy from containers and spawn instead of sources
+    • This will also mean I can have a group of harvesters that can fill spawn and containers
+    and the rest can just collect energy from there to use
+• figure out how to streamline the creeps' targets:
+    selecting target
+    saving target name in memory
+    doing something with target until that thing is done
+    only then re-processing to find new target
+
+*/
+
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
@@ -7,6 +23,23 @@ var spawnNewCreep = require('action.spawnCreep');
 
 module.exports.loop = function () {
 
+    var tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        /*
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+        */
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
     for (var name in Memory.creeps) {
         if (!Game.creeps[name]) {
             delete Memory.creeps[name];
@@ -15,11 +48,12 @@ module.exports.loop = function () {
     }
 
     spawnNewCreep.run('harvester', 2, [WORK, WORK, WORK, CARRY, MOVE]);
-    spawnNewCreep.run('builder', 3, [WORK, CARRY, MOVE]);
+    spawnNewCreep.run('builder', 5, [WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
     spawnNewCreep.run('repairer', 3, [WORK, CARRY, MOVE]);
     spawnNewCreep.run('upgrader', 6, [WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
     spawnNewCreep.run('mason', 10, [WORK, CARRY, MOVE, MOVE, MOVE]);
 
+    // streamline this with a loop
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
         if (creep.memory.role == 'harvester') {
