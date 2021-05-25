a6800e1c0fc2ac76e494e247e941f45151dd152f
Gerald Southwood <72921662+VertX-G@users.noreply.github.com>
2021-05-25 16:24:33 +0200

    Added stage three creep body parts

--- main.js
+++ main.js
@@ -108,6 +108,7 @@ module.exports.loop = function () {
         spawnNewCreep.run('mason', 3, [WORK, WORK, CARRY, MOVE, MOVE]);
         */
 
+                /*
         // Second Stage Creeps
         // in reverse order of importance because last one called will be spawned
         spawnNewCreep.run('artillery', 1, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
@@ -116,7 +117,32 @@ module.exports.loop = function () {
         spawnNewCreep.run('repairer', 2, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
         spawnNewCreep.run('upgrader', 4, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
         spawnNewCreep.run('harvester', 4, [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE]);
+        */
+        
+        // Third Stage Creeps
+        // in reverse order of importance because last one called will be spawned
+        spawnNewCreep.run('artillery', 1, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]);
+        spawnNewCreep.run('mason', 2, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]);
+        spawnNewCreep.run('builder', 3, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]);
+        spawnNewCreep.run('repairer', 2, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]);
+        spawnNewCreep.run('upgrader', 4, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]);
+        spawnNewCreep.run('harvester', 4, [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]);
     }
+    /*
+    ---------- BODY PART COST ----------
+    Body part	Build cost	Effect
+    MOVE	        50	    Moves the creep. Reduces creep fatigue by 2/tick. See movement.
+    WORK	        100	    Harvests energy from target source. Gathers 2 energy/tick.
+                            Constructs a target structure. Builds the designated structure at a construction site, at 5 points/tick, consuming 1 energy/point. See building Costs.
+                            Repairs a target structure. Repairs a structure for 100 hits/tick. Consumes 0.01 energy/hit repaired, rounded up to the nearest whole number.
+    CARRY	        50	    Stores energy. Contains up to 50 energy units. Weighs nothing when empty.
+    ATTACK	        80	    Attacks a target creep/structure. Deals 30 damage/tick. Short-ranged attack (1 tile).
+    RANGED_ATTACK	150	    Attacks a target creep/structure. Deals 10 damage/tick. Long-ranged attack (1 to 3 tiles).
+    HEAL	        250	    Heals a target creep. Restores 12 hit points/tick at short range (1 tile) or 4 hits/tick at a distance (up to 3 tiles).
+    TOUGH	        10	    No effect other than the 100 hit points all body parts add. This provides a cheap way to add hit points to a creep.
+    CLAIM	        600	
+    */
+    
 
     var currentlySpawning = Game.spawns['Spawn1'].spawning;
     if (currentlySpawning != null && currentlySpawning.remainingTime == currentlySpawning.needTime -1) {
